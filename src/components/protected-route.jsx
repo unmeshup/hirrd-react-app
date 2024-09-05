54a3650e4229779f4c96bc3babd5ddb1811a4573
unmeshup <unplfc@gmail.com>
2024-09-05 20:03:24 +0530

    Authentication Issues Solved

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,20 +1,26 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
+import { ClipLoader } from "react-spinners";
 
 const ProtectedRoute = ({ children }) => {
   const { isSignedIn, isLoaded, user } = useUser();
   const { pathname } = useLocation();
 
-  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+  if (!isLoaded) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <ClipLoader color="#6366f1" size={50} />
+      </div>
+    );
+  }
+
+  if (!isSignedIn) {
     return <Navigate to="/?sign-in=true" />;
   }
 
-  if (
-    user !== undefined &&
-    !user?.unsafeMetadata?.role &&
-    pathname !== "/onboarding"
-  )
+  if (!user?.unsafeMetadata?.role && pathname !== "/onboarding") {
     return <Navigate to="/onboarding" />;
+  }
 
   return children;
 };
