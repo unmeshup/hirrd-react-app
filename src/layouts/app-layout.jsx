446596303b2da3e1fac5f8a9ca7fcb5f5ef0de09
unmeshup <unplfc@gmail.com>
2024-09-05 18:55:36 +0530

    Ready for Deployment

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/header";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <div className="grid-background"></div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Made with 💗 by Unmesh
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
